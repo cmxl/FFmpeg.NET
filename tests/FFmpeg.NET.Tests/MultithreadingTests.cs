62ef1e3b37f1e905e68f27c0f45ce1594df47119
Yago Tomé <yago.tome@hotmail.com>
2018-09-08 13:17:28 -0300

    Fix multithreading tests by disabling parallelism on test collections

--- tests/FFmpeg.NET.Tests/MultithreadingTests.cs
+++ tests/FFmpeg.NET.Tests/MultithreadingTests.cs
@@ -51,17 +51,16 @@ namespace FFmpeg.NET.Tests
         [Fact]
         public void Multiple_FFmpeg_Instances_At_Once_Do_Not_Throw_Exception()
         {
-            // TODO Verify why following commented code throws exception
-            //var task1 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
-            //var task2 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
-            //var task3 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
-            //Task.WaitAll(task1, task2, task3);
+            var task1 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
+            var task2 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
+            var task3 = Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); });
+            Task.WaitAll(task1, task2, task3);
 
             Task.Run(() => { new Engine.FFmpeg().GetMetaData(_fixture.VideoFile); }).Wait();
 
-            var task1 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
-            var task2 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
-            var task3 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
+            task1 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
+            task2 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
+            task3 = new Engine.FFmpeg().GetMetaDataAsync(_fixture.VideoFile);
             Task.WaitAll(task1, task2, task3);
 
             new Engine.FFmpeg().GetMetaData(_fixture.VideoFile);
