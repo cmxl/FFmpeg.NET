3707dbdb0ca7f3f557910e2e75e75af8c7f3ea11
tobias.haimerl <tobias.haimerl@conceptnet.de>
2018-11-20 09:41:01 +0100

    [skip ci] add sample project

---
+++ samples/FFmpeg.NET.Sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using FFmpeg.NET.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace FFmpeg.NET.Sample
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var inputFile = new MediaFile(@"..\..\..\..\..\tests\FFmpeg.NET.Tests\MediaFiles\SampleVideo_1280x720_1mb.mp4");
+            var outputFile = new MediaFile(@"output.mkv");
+
+            var ffmpeg = new Engine.FFmpeg(@"..\..\..\..\..\lib\ffmpeg\v4\ffmpeg.exe");
+            ffmpeg.Progress += OnProgress;
+            ffmpeg.Data += OnData;
+            ffmpeg.Error += OnError;
+            ffmpeg.Complete += OnComplete;
+            await ffmpeg.ConvertAsync(inputFile, outputFile);
+            Console.ReadLine();
+        }
+
+        private static void OnProgress(object sender, ConversionProgressEventArgs e)
+        {
+            Console.WriteLine("[{0} => {1}]", e.Input.FileInfo.Name, e.Output.FileInfo.Name);
+            Console.WriteLine("Bitrate: {0}", e.Bitrate);
+            Console.WriteLine("Fps: {0}", e.Fps);
+            Console.WriteLine("Frame: {0}", e.Frame);
+            Console.WriteLine("ProcessedDuration: {0}", e.ProcessedDuration);
+            Console.WriteLine("Size: {0} kb", e.SizeKb);
+            Console.WriteLine("TotalDuration: {0}\n", e.TotalDuration);
+        }
+
+        private static void OnData(object sender, ConversionDataEventArgs e)
+            => Console.WriteLine("[{0} => {1}]: {2}", e.Input.FileInfo.Name, e.Output.FileInfo.Name, e.Data);
+
+        private static void OnComplete(object sender, ConversionCompleteEventArgs e) 
+            => Console.WriteLine("Completed conversion from {0} to {1}", e.Input.FileInfo.FullName, e.Output.FileInfo.FullName);
+
+        private static void OnError(object sender, ConversionErrorEventArgs e) 
+            => Console.WriteLine("[{0} => {1}]: Error: {2}\n{3}", e.Input.FileInfo.Name, e.Output.FileInfo.Name, e.Exception.ExitCode, e.Exception.InnerException);
+    }
+}
