a37b8e71002d14798c0a6f4f65164008063c6888
martinyyyy <freaksey@gmail.com>
2019-11-06 18:18:40 +0100

    Duration parsing fixed (#23)

--- src/FFmpeg.NET/RegexEngine.cs
+++ src/FFmpeg.NET/RegexEngine.cs
@@ -92,7 +92,8 @@ namespace FFmpeg.NET
             // Process seconds
             double seconds = 0.0;
             start = end + 1;
-            if (!double.TryParse(str.Substring(start), out seconds))
+            // ffmpeg doesnt respect the computers culture
+            if (!double.TryParse(str.Substring(start), NumberStyles.Number, CultureInfo.InvariantCulture, out seconds))
                 return false;
 
             result = new TimeSpan(0, hours, minutes, 0, (int)Math.Round(seconds * 1000.0));
