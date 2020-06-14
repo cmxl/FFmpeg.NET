c6e64693a1e6620dbed41009c9a3dd55ec57b9ed
Nicolas Beltran <nicolas.beltran.g@gmail.com>
2020-06-14 10:20:51 -0500

    - Added Several Options for convertion - Added Enums for some of the video convertion options - Fixed some memory leaks for continuous use of FFmpegProcess (e.g. Function Pointers unsubscribing)

---
+++ src/FFmpeg.NET/Enums/HWAccel.cs
@@ -0,0 +1,11 @@
+﻿namespace FFmpeg.NET.Enums
+{
+    public enum HWAccel
+    {
+        None,
+        cuda,
+        cuvid,
+        dxva2,               //Direct-X Video Acceleration API, developed by Microsoft (supports Windows and XBox360).
+        qsv,
+        d3d11va}
+}
---
+++ src/FFmpeg.NET/Enums/VideoCodecProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace FFmpeg.NET.Enums
+{
+    public enum VideoCodecProfile
+    {
+        Default,
+        baseline,
+        main,
+        high,
+        high10,
+        high422,
+        high444
+    }
+}
